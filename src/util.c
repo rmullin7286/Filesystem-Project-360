cdaa48fc6504fa6ca057fb7856f6ec62f9e567d0
dguzman <darren.guzman@wsu.edu>
2018-12-03 14:42:07 -0800

    Nice.

--- src/util.c
+++ src/util.c
@@ -49,6 +49,14 @@ int put_block(int dev, int blk, char *buf)
 int tokenize(char *pathname)
 {
   // tokenize pathname into n components: name[0] to name[n-1];
+    char * temp;
+    int i = 0;
+    while(temp = strtok(pathname, "/") && i < 64)
+    {
+      name[i] = temp;
+      i++;
+    }
+    return i;
 }
 
 MINODE *iget(int dev, int ino)
@@ -95,6 +103,31 @@ int iput(MINODE *mip) // dispose a used minode by mip
 // serach a DIRectory INODE for entry with a given name
 int search(MINODE *mip, char *name)
 {
+  char dbuf[BLKSIZE], temp[256];
+  DIR *dp;
+  char * cp;
+  for (int i = 0; i < 12; i++)
+  {
+    if (ip->i_block[i] == 0)
+      break;
+    get_block(fd, ip->i_block[i], dbuf);
+    dp = (DIR*) dbuf;
+    cp = dbuf;
+
+    while (cp < dbuf + BLKSIZE)
+    {
+      strncpy(temp, dp->name, dp->name_len);
+      temp[dp->name_len] = 0;
+      
+      if(strncmp(name,dp->name, dp->name_len) == 0)
+      {
+        return dp->inode;
+      }
+      cp += dp->rec_len;
+      dp = (DIR*)cp;
+    }
+  }
+  return 0;
    // return ino if found; return 0 if NOT
 }
 
@@ -118,9 +151,3 @@ int findmyname(MINODE *parent, u32 myino, char *myname)
    // copy entry name (string) into myname[ ];
 }
 
-
-int findino(MINODE *mip, u32 *myino) 
-{
-  // fill myino with ino of . 
-  // retrun ino of ..
-}
