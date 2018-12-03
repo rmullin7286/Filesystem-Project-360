a140cc4a260e174d47abb4e1c6dc9f184aadb883
Ryan Mullin <ryan.mullin@wsu.edu>
2018-12-03 13:52:09 -0800

    added KC's code

--- src/util.c
+++ src/util.c
@@ -1,35 +1,118 @@
-#include "util.h"
 
-#include <unistd.h>
-#include <stdint.h>
+/*********************************************************************
+        You MAY use the util.o file to get a quick start.
+ BUT you must write YOUR own util.c file in the final project
+ The following are helps of how to wrtie functions in the util.c file
+*********************************************************************/
 
-void get_block(int fd, int blk, char buf[ ])
+
+/************** util.c file ****************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <ext2fs/ext2_fs.h>
+#include <string.h>
+#include <libgen.h>
+#include <sys/stat.h>
+
+#include "type.h"
+
+/**** globals defined in main.c file ****/
+
+extern MINODE minode[NMINODE];
+extern MINODE *root;
+extern PROC   proc[NPROC], *running;
+extern char gpath[128];
+extern char *name[64];
+extern int n;
+extern int fd, dev;
+extern int nblocks, ninodes, bmap, imap, inode_start;
+extern char line[256], cmd[32], pathname[256];
+
+
+int get_block(int dev, int blk, char *buf)
 {
-  lseek(fd, blk*BLKSIZE, 0);
-  read(fd, buf, BLKSIZE);
+}   
+
+int put_block(int dev, int blk, char *buf)
+{
+}   
+
+int tokenize(char *pathname)
+{
+  // tokenize pathname into n components: name[0] to name[n-1];
 }
 
-void put_block(int fd, int blk, char buf[])
+MINODE *iget(int dev, int ino)
 {
-    lseek(fd, blk*BLKSIZE, 0);
-    write(fd, buf, BLKSIZE);
+  // return minode pointer to loaded INODE
+  (1). Search minode[ ] for an existing entry (refCount > 0) with 
+       the needed (dev, ino):
+       if found: inc its refCount by 1;
+                 return pointer to this minode;
+
+  (2). // needed entry not in memory:
+       find a FREE minode (refCount = 0); Let mip-> to this minode;
+       set its refCount = 1;
+       set its dev, ino
+
+  (3). load INODE of (dev, ino) into mip->INODE:
+       
+       // get INODE of ino a char buf[BLKSIZE]    
+       blk    = (ino-1) / 8 + inode_start;
+       offset = (ino-1) % 8;
+
+       printf("iget: ino=%d blk=%d offset=%d\n", ino, blk, offset);
+
+       get_block(dev, blk, buf);
+       ip = (INODE *)buf + offset;
+       mip->INODE = *ip;  // copy INODE to mp->INODE
+
+       return mip;
 }
 
-void read_indirect_block(int fd, int blk, int indirect, char buf[])
+
+int iput(MINODE *mip) // dispose a used minode by mip
 {
-    uint32_t ibuf[INDIRECT_BLOCK_LENGTH];
-    get_block(fd, blk, (char*)ibuf);
-    blk = ibuf[indirect];
-    get_block(fd, blk, buf);
-    
+ mip->refCount--;
+ 
+ if (mip->refCount > 0) return;
+ if (!mip->dirty)       return;
+ 
+ // Write YOUR CODE to write mip->INODE back to disk
+
+} 
+
+
+// serach a DIRectory INODE for entry with a given name
+int search(MINODE *mip, char *name)
+{
+   // return ino if found; return 0 if NOT
 }
 
-void read_double_indirect(int fd, int blk, int indirect, int indirect2, char buf[])
+
+// retrun inode number of pathname
+
+int getino(char *pathname)
+{ 
+   // SAME as LAB6 program: just return the pathname's ino;
+}
+
+
+
+// THESE two functions are for pwd(running->cwd), which prints the absolute
+// pathname of CWD. 
+
+int findmyname(MINODE *parent, u32 myino, char *myname) 
 {
-    uint32_t ibuf[INDIRECT_BLOCK_LENGTH];
-    get_block(fd, blk, (char*)ibuf);
-    blk = ibuf[indirect];
-    get_block(fd, blk, (char*)ibuf);
-    blk = ibuf[indirect2];
-    get_block(fd, blk, buf);
+   // parent -> at a DIR minode, find myname by myino
+   // get name string of myino: SAME as search except by myino;
+   // copy entry name (string) into myname[ ];
+}
+
+
+int findino(MINODE *mip, u32 *myino) 
+{
+  // fill myino with ino of . 
+  // retrun ino of ..
 }
